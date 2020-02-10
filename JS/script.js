313a549e42f98871362d35bd9c33df5c67c80db4
miguelcarmona23 <miguelcarmona996@gmail.com>
2020-02-09 20:00:26 -0500

    Added const imageBottom, isHalfShown, isNotScrolledPast and added an if statement

--- JS/script.js
+++ JS/script.js
@@ -18,7 +18,18 @@ function debounce(func, wait = 20, immediate = true) {
 const sliderImages = document.querySelectorAll('.slide-in');
 
 function checkSlide(e) {
-
+    sliderImages.forEach(sliderImage => {
+        const SlideInAt = (window.scrollY + window.innerHeight) -
+            sliderImage.height / 2;
+        const imageBottom = sliderImage.offsetTop + sliderImage.height;
+        const isHalfShown = SlideInAt > sliderImage.offsetTop;
+        const isNotScrolledPast = window.scrollY < imageBottom;
+        if (isHalfShown && isNotScrolledPast) {
+            sliderImage.classList.add('active');
+        } else {
+            sliderImage.classList.remove('active');
+        }
+    });
 }
 
 window.addEventListener('scroll', debounce(checkSlide));
